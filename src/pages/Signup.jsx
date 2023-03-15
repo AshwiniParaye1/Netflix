f8ea7af9ab9ae2293d9a8895b6240e5bac7e1775
Ashwini <ashwiniparaye9309@gmail.com>
2023-03-15 18:34:29 +0530

    modified

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,8 +7,10 @@ export default function Signup() {
   return (
     <Container>
       <BackgroundImage />
-      <Header />
-      <div className="body flex column a-center j-center">
+      <div className="content">
+        <Header />
+        
+        <div className="body flex column a-center j-center">
         <div className="text flex column">
           <h1>Unlimited movies, TV shows and more</h1>
           <h4>Watch anywhere, Cancel anytime.</h4>
@@ -22,8 +24,20 @@ export default function Signup() {
         </div>
         <button>Log In</button>
       </div>
+      </div>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+position: relative;
+.content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,0.5);
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 15vh 85vh;
+`;
