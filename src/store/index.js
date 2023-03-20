eb26f5d424708b4243e35d25219fc42eb3fd32cf
Ashwini <ashwiniparaye9309@gmail.com>
2023-03-20 19:36:52 +0530

    genres fetched

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { configureStore, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { API_KEY, TMDB_BASE_URL } from "../utils/constants";
 
 
 
@@ -8,10 +10,25 @@ const initialState = {
     genres: [],
 };
 
+export const getGenres = createAsyncThunk("netflix/genres", 
+async () => {
+    const { data } = await axios.get(
+        `${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`);
+
+        console.log(data);
+        // return data;
+});
+
+
 const NetflixSlice = createSlice({
     name: "Netflix",
     initialState,
-    extraReducers: (builder) => {},
+    extraReducers: (builder) => {
+        builder.addCase(getGenres.fulfilled, (state, action) => {
+            state.genres = action.payload;
+            state.genresLoaded = true;
+        })
+    },
 });
 
 export const store = configureStore({
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = "f6de5422c2c42314a865d48be52b38aa";
-export const TMDB_BASE_URL = "https://www.themoviedb.org/3";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
