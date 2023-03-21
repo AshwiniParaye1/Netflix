5914dd49ecf2abea22fe45215f6ce3882efb4a43
Ashwini <ashwiniparaye9309@gmail.com>
2023-03-21 12:49:41 +0530

    added comments

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import backgroundImage from '../assets/home.jpg';
 import MovieLogo from '../assets/homeTitle.webp';
@@ -6,42 +6,46 @@ import { FaPlay } from 'react-icons/fa';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getGenres } from './../store/index';
 
 
-export default function Netflix() {
 
-  const navigate = useNavigate()
+export default function Netflix() {  // define a React component called Netflix
 
-  const [ isScrolled, setIsScrolled ] = useState(false)
+  const navigate = useNavigate()  // use the useNavigate hook from react-router-dom to handle navigation
 
-  const dispatch = useDispatch();
-  
-  useEffect( () => {
+  const [ isScrolled, setIsScrolled ] = useState(false)  // define a state variable called isScrolled to keep track of whether the page has been scrolled or not
+
+  const dispatch = useDispatch();  // use the useDispatch hook from react-redux to dispatch actions
+
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);  // use the useSelector hook from react-redux to select a piece of state
+
+  useEffect( () => {  // use the useEffect hook to fetch data when the component mounts
     dispatch(getGenres())
   }, [] )
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
+  window.onscroll = () => {  // set a window event listener to detect when the page is scrolled
+    setIsScrolled(window.pageYOffset === 0 ? false : true);  // set isScrolled to true if the page has been scrolled, otherwise false
+    return () => (window.onscroll = null);  // remove the event listener when the component unmounts
   }
 
-  return (
-    <Container>
-      <Navbar isScrolled={isScrolled} />
-      <div className='hero'>
-        <img src={backgroundImage} alt='background' className='background-image' />
-        <div className='container'>
-          <div className='logo'>
-            <img src={MovieLogo} alt='Movie Logo' />
+  return (  // return the JSX markup for the component
+    <Container>  
+      <Navbar isScrolled={isScrolled} /> 
+      <div className='hero'>  
+        <img src={backgroundImage} alt='background' className='background-image' />  
+        <div className='container'>  
+          <div className='logo'>  
+            <img src={MovieLogo} alt='Movie Logo' /> 
           </div>
-          <div className='buttons flex'>
-            <button className='flex j-center a-center' onClick={() => navigate('/player')}>
+          <div className='buttons flex'> 
+            <button className='flex j-center a-center' onClick={() => navigate('/player')}>  
+            
               <FaPlay />Play
             </button>
-            <button className='flex j-center a-center'>
-              <AiOutlineInfoCircle />More Info
+            <button className='flex j-center a-center'>  
+                <AiOutlineInfoCircle />More Info
             </button>
           </div>
         </div>
