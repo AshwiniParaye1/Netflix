3c74c87674aea7977217b3c53817125c08135fb2
Ashwini <ashwiniparaye9309@gmail.com>
2023-03-24 12:25:13 +0530

    styled slider

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -11,7 +11,16 @@ export default function CardSlider( { data, title } ) {
   const listRef = useRef();
 
   const handleDirection = (direction) => {
-
+    console.log('listref ===', listRef, direction)
+    let distance = listRef.current.getBoundingClientRect().x-70
+    if(direction === 'left' && sliderPosition > 0) {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      setSliderPosition(sliderPosition - 1)
+    }
+    if(direction === 'right' && sliderPosition < 4) {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      setSliderPosition(sliderPosition + 1)
+    }
   }
 
   return (
@@ -44,4 +53,43 @@ export default function CardSlider( { data, title } ) {
 )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+
+gap: 1rem;
+position: relative;
+padding: 2rem 0;
+h1 {
+  margin-left: 50px;
+}
+.wrapper {
+  .slider {
+    width: max-content;
+    gap: 1rem;
+    transform: translateX(0px);
+    transition: 0.3s ease-in-out;
+    margin-left: 50px;
+  }
+  .slider-action {
+    position: absolute;
+    z-index: 99;
+    height: 100%;
+    top: 0;
+    bottom: 0;
+    width: 50px;
+    transition: 0.3s ease-in-out;
+    svg{
+      font-size: 2rem ;
+    }
+  }
+  .none {
+      display:none;      
+    }
+    .left {
+      left: 0;
+    }
+    .right {
+      right: 0;
+    }
+}
+
+`;
